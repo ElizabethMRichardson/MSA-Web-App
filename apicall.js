dbce69ff27510130dd3cb4367514158e34735eee
Liz Richardson <Looga@Lizs-MacBook-Pro.local>
2016-09-11 19:47:51 +1200

    compiled ts whoops

--- apicall.js
+++ apicall.js
@@ -12,28 +12,44 @@ function apiConnection() {
     var obj = JSON.parse(result);
     var city = obj.name;
     var test = obj.main.temp;
+    var id = obj.weather.id;
     // Modify text of HTML paragraph to display text returned by the API
     //id of paragraph- HTML element adding the api to
-    fillHtml(test);
+    fillHtml(test, id);
     //document.getElementById("result").innerHTML = result;
     document.getElementById("test").innerHTML = test;
 }
 ;
-function fillHtml(temperature) {
+function fillHtml(temperature, weather) {
     document.getElementById("temp-heading").innerHTML = "The temperature is";
     document.getElementById("temperature").innerHTML = temperature;
-    if (temperature <= 10) {
-        $('html').css('background', 'url(http://hdimagesnew.com/wp-content/uploads/2015/11/Snow-Wallpapers-9.jpg)');
+    if (weather == 961 || weather == 901 || weather == 960 || (weather >= 200 && weather < 300)) {
+        $('html').css('background', 'url(https://upload.wikimedia.org/wikipedia/commons/c/c2/Port_and_lighthouse_overnight_storm_with_lightning_in_Port-la-Nouvelle.jpg)');
         playSound('song1.mp3');
     }
-    else if (temperature > 10 && temperature < 20) {
-        $('html').css('background', 'url(http://api.ning.com/files/4zDRwQPj*wvbJIcxs69xMfKLdYDzvlY8yQ4-uOAoOwMkwzywLmeausNISnLHX02P8X8uO4kZYKnObGUWG**dTOW6RoqNBHo-/DSC02494.JPG)');
+    else if (weather >= 300 && weather < 400) {
+        $('html').css('background', 'url(https://static.pexels.com/photos/68357/pexels-photo-68357.jpeg)');
     }
-    else if (temperature >= 20 && temperature < 30) {
-        $('html').css('background', 'url(http://feelgrafix.com/data_images/out/15/899365-sunny-day-wallpaper.jpg)');
+    else if ((weather >= 500 && weather < 600) || weather == 906) {
+        $('html').css('background', 'url(https://upload.wikimedia.org/wikipedia/commons/6/64/Guy_with_an_umbrella_battling_rain_and_snow_on_a_bridge.jpg)');
+    }
+    else if ((weather >= 600 && weather < 699) || weather == 903) {
+        $('html').css('background', 'url(https://static.pexels.com/photos/5313/cold-snow-nature-forest.jpg)');
+    }
+    else if ((weather >= 700 && weather < 799)) {
+        $('html').css('background', 'url(https://static.pexels.com/photos/3176/mountains-forest-fog-mist.jpeg)');
+    }
+    else if (weather == 800 || weather == 904) {
+        $('html').css('background', 'url(https://static.pexels.com/photos/96389/pexels-photo-96389.jpeg)');
+    }
+    else if ((weather >= 801 && weather <= 804)) {
+        $('html').css('background', 'url(https://static.pexels.com/photos/1818/sky-clouds-cloudy-forest.jpg)');
+    }
+    else if ((weather >= 952 && weather < 960) || weather == 962 || weather == 905 || weather == 900 || weather == 902) {
+        $('html').css('background', 'url(http://www.publicdomainpictures.net/pictures/10000/velka/87-12681332789SQ8.jpg)');
     }
     else {
-        $('html').css('background', 'url(http://webneel.com/wallpaper/sites/default/files/images/08-2013/10-star-fish-sea-beach-sand-wallpaper.jpg)');
+        $('html').css('background', 'url(https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/Flag_of_Afghanistan_(1880%E2%80%931901).svg/2000px-Flag_of_Afghanistan_(1880%E2%80%931901).svg.png)');
     }
 }
 function playSound(url) {
