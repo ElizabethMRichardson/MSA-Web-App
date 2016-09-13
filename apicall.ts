bb315aabb142d3b669e673886b088cab0782aa8f
Liz Richardson <Looga@Lizs-MacBook-Pro.local>
2016-09-13 23:28:09 +1200

    doesn't seem to be getting link  between github and azure. Doing another push to see.

--- apicall.ts
+++ apicall.ts
@@ -29,60 +29,80 @@ function apiConnection() {
 };
 
 function fillHtml(temperature, weather, desc){
+
+    var spotifySrc;
     
 
 
-    document.getElementById("temp-heading").innerHTML = "The temperature is";
-    document.getElementById("temperature").innerHTML = temperature;
-    document.getElementById("test-id").innerHTML = desc;
+    document.getElementById("temp-heading").innerHTML = "The temperature is " + temperature + " and the weather is " + desc;
+    //document.getElementById('spotify').src = "https://embed.spotify.com/?uri=spotify:user:1270362959:playlist:1CPOj48vGYYzNWNql42ic9";
 
+//Thunderstorm
     if(weather == 961 || weather == 901 || weather == 960 || (weather >= 200 && weather < 300)){
         $('html').css('background', 'url(https://upload.wikimedia.org/wikipedia/commons/c/c2/Port_and_lighthouse_overnight_storm_with_lightning_in_Port-la-Nouvelle.jpg)');
-       $('html').css('background-size', 'cover');
-       // document.body.style.backgroundImage = "url('http://webneel.com/wallpaper/sites/default/files/images/08-2013/10-star-fish-sea-beach-sand-wallpaper.jpg')";
+        $('html').css('background-size', 'cover');
+        spotifySrc = "https://embed.spotify.com/?uri=spotify:user:spotify:playlist:7MPXsjQGsXcLdNz7UxVuVl&theme=white";
+       
     }
+    //Drizzle
     else if(weather >= 300 && weather < 400){
         $('html').css('background', 'url(https://static.pexels.com/photos/68357/pexels-photo-68357.jpeg)');
         $('html').css('background-size', 'cover');
+        spotifySrc = "https://embed.spotify.com/?uri=spotify:user:spotify:playlist:7CQunpJEHecknIyABfS8pP&theme=white";
     }
+
+    //Rain
     else if((weather >= 500 && weather < 600) || weather == 906){
         $('html').css('background', 'url(https://upload.wikimedia.org/wikipedia/commons/6/64/Guy_with_an_umbrella_battling_rain_and_snow_on_a_bridge.jpg)');
         $('html').css('background-size', 'cover');
+        spotifySrc = "https://embed.spotify.com/?uri=spotify:user:spotify:playlist:7CQunpJEHecknIyABfS8pP&theme=white";
     }
+
+    //Snow
     else if((weather >= 600 && weather < 699) || weather == 903){
          $('html').css('background', 'url(https://static.pexels.com/photos/5313/cold-snow-nature-forest.jpg)');
          $('html').css('background-size', 'cover');
+         spotifySrc = "https://embed.spotify.com/?uri=spotify:user:spotifyaustralia:playlist:08JJx9duO6zKYSV4J2I2hL&theme=white";
 
     }
+
+    //Fog
     else if((weather >= 700 && weather < 799)){
         $('html').css('background', 'url(https://static.pexels.com/photos/3176/mountains-forest-fog-mist.jpeg)');
         $('html').css('background-size', 'cover');
+        spotifySrc="https://embed.spotify.com/?uri=spotify:album:0yc9LsCdJFBZoBdRKoNQyB&theme=white"; 
     }
+
+    //clear
     else if(weather == 800 || weather == 904){
         $('html').css('background', 'url(http://s3.freefoto.com/images/9909/05/9909_05_6082_web.jpg) no-repeat center center fixed');
         $('html').css('background-size', 'cover');
+        spotifySrc = "https://embed.spotify.com/?uri=spotify:user:spotify:playlist:1KuPMhQ4z7oIq3zdQEZP0V&theme=white";
     }
+
+    //Clouds
     else if((weather >= 801 && weather <= 804)){
         $('html').css('background', 'url(https://static.pexels.com/photos/1818/sky-clouds-cloudy-forest.jpg)');
+        spotifySrc = "https://embed.spotify.com/?uri=spotify:user:spotify_france:playlist:2uIgi9q0HmyhRj9gRRb5D7&theme=white";
     }
+
+    //Wind
     else if((weather >= 952 && weather < 960) || weather == 962 || weather == 905 || weather == 900 || weather == 902){
         $('html').css('background', 'url(http://www.publicdomainpictures.net/pictures/10000/velka/87-12681332789SQ8.jpg)');
         $('html').css('background-size', 'cover');
+        spotifySrc = "https://embed.spotify.com/?uri=spotify:user:solodoloalan:playlist:4Wy97advUlh6i3BUp6sZnk&theme=white";
     }
+
+    //Other
     else{
         $('html').css('background', 'url(https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/Flag_of_Afghanistan_(1880%E2%80%931901).svg/2000px-Flag_of_Afghanistan_(1880%E2%80%931901).svg.png)');
         $('html').css('background-size', 'cover');
+        spotifySrc = "https://embed.spotify.com/?uri=spotify:user:spotify_uk_:playlist:3V1WI57CMyQdmxy3aibCB4&theme=white";
     }
 
-}
+    document.getElementById("temp-heading").innerHTML = "The temperature is " + temperature + " and the weather is " + desc;
 
-function playSound(url){
-  var audio = document.createElement('audio');
-  audio.style.display = "none";
-  audio.src = url;
-  audio.autoplay = true;
-  audio.onended = function(){
-    audio.remove() //Remove when played.
-  };
-  document.body.appendChild(audio);
+    $('#spotify').attr('src', spotifySrc);
+    
+
 }
